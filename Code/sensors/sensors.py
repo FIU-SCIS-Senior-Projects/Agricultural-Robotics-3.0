1f90724a29131d3957c194d91f1c040343ce83c6
Sean Monroe <seanmonroe86@gmail.com>
2017-05-23 12:34:40 -0400

    GPS default values acquired

--- Code/sensors/sensors.py
+++ Code/sensors/sensors.py
@@ -8,4 +8,19 @@ time.sleep(0.5)
 battery = drone.getBattery()
 drone.printBlue("Battery: {}% {}".format(battery[0], battery[1]))
 
+drone.useDemoMode(False)
+CDC = drone.ConfigDataCount
+drone.getConfig()
+while CDC == drone.ConfigDataCount: time.sleep(0.001)
+lat, lon, alt = 0, 0, 0
+for conf in drone.ConfigData:
+    if conf[0] == "gps:latitude":
+        lat = conf[1]
+    elif conf[0] == "gps:longitude":
+        lon = conf[1]
+    elif conf[0] == "gps:altitude":
+        alt = conf[1]
+
+print "{}\n{}\n{}".format(lat, lon, alt)
+
 
