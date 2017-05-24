a5615d09019904dfe8f9140543ccd0012ff4c099
Sean Monroe <seanmonroe86@gmail.com>
2017-05-24 18:41:14 -0400

    magnetometer, gps, wifi sensor data

--- Code/sensors/sensors.py
+++ Code/sensors/sensors.py
@@ -1,26 +1,36 @@
-import ps_drone, time
+import ps_drone, time, sys
 
+# initialize drone
 drone = ps_drone.Drone()
 drone.startup()
 drone.reset()
-while drone.getBattery()[0] == -1: time.sleep(0.1)
-time.sleep(0.5)
+drone.useDemoMode(False)
+
+# print starting battery
+timer = time.time()
+while drone.getBattery()[0] == -1:
+    time.sleep(0.1)
+    timer = time.time()
+    if timer > 2:
+        print "bleh"
+        sys.exit()
 battery = drone.getBattery()
 drone.printBlue("Battery: {}% {}".format(battery[0], battery[1]))
 
-drone.useDemoMode(False)
-CDC = drone.ConfigDataCount
-drone.getConfig()
-while CDC == drone.ConfigDataCount: time.sleep(0.001)
-lat, lon, alt = 0, 0, 0
-for conf in drone.ConfigData:
-    if conf[0] == "gps:latitude":
-        lat = conf[1]
-    elif conf[0] == "gps:longitude":
-        lon = conf[1]
-    elif conf[0] == "gps:altitude":
-        alt = conf[1]
+# gps, magnetometer, wifi connection
+#while (package for package in ["gps","magneto","wifi"]) not in drone.NavData:
+while any(package not in drone.NavData for package in ["gps","magneto","wifi"]):
+    NDC = drone.NavDataCount
+    drone.getNDpackage(["all"])
+    while drone.NavDataCount == NDC: time.sleep(0.001)
 
-print "{}\n{}\n{}".format(lat, lon, alt)
+mag = drone.NavData["magneto"][0]
+gps = drone.NavData["gps"]
+wif = drone.NavData["wifi"]
+
+print "{}\n{}\n{}".format(mag, gps, wif)
 
 
+
+
+drone.shutdown()
