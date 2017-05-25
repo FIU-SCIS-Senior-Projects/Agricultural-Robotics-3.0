604381592c2a5d56df74a3b0f4316901b1bcf3b8
Sean Monroe <seanmonroe86@gmail.com>
2017-05-25 11:43:41 -0400

    testing magnetometer measurements

--- Code/sensors/sensors.py
+++ Code/sensors/sensors.py
@@ -18,17 +18,19 @@ battery = drone.getBattery()
 drone.printBlue("Battery: {}% {}".format(battery[0], battery[1]))
 
 # gps, magnetometer, wifi connection
-#while (package for package in ["gps","magneto","wifi"]) not in drone.NavData:
 while any(package not in drone.NavData for package in ["gps","magneto","wifi"]):
     NDC = drone.NavDataCount
     drone.getNDpackage(["all"])
     while drone.NavDataCount == NDC: time.sleep(0.001)
 
-mag = drone.NavData["magneto"][0]
-gps = drone.NavData["gps"]
-wif = drone.NavData["wifi"]
+mag = drone.NavData["magneto"]
+#print "{}\n{}\n{}\n{}".format(mag[0],mag[1],mag[2],mag[3])
+#print "{}\n{}\n{}\n{}".format(mag[4],mag[5],mag[6],mag[7])
+#print "{}\n{}\n{}\n{}".format(mag[8],mag[9],mag[10],mag[11])
 
-print "{}\n{}\n{}".format(mag, gps, wif)
+#gps = drone.NavData["gps"]
+#wif = drone.NavData["wifi"]
+#print "{}\n{}\n{}".format(mag, gps, wif)
 
 
 
