6cc1e75c4dcae72cfbf54fc32b573beefbfa534d
chris <csilv069@fiu.edu>
2017-05-28 12:36:56 -0400

    Defined controller functions

--- Code/flight_controller.py
+++ Code/flight_controller.py
@@ -1,6 +1,60 @@
-
-
+from Tkinter import *
+import ps_drone
 
 # Drone flight_controller
 
+class Controller():
+	
+	def take_off():
+		print "Clicked takeoff"
 
+	def stop_moving():
+		print "Stop moving"
+
+	def move_forward():
+		print "Move forward"
+
+	def move_backward():
+		print "Move backward"
+
+	def move_left():
+		print "Move left"
+
+	def move_right():
+		print "Move right"
+
+	def turn_left():
+		print "Turn left"
+
+	def turn_right():
+		print "Turn right"
+
+
+
+	main = Tk()
+	main.title("D.F.C. - Drone Flight Controller")
+	frame = Frame(main)
+	frame.pack()	
+
+	main.bind('<space>', take_off())
+	main.bind('0', stop_moving())
+	main.bind('w', move_forward())
+	main.bind('s', move_backward())
+	main.bind('a', move_left())
+	main.bind('d', move_right())
+	main.bind('q', turn_left())
+	main.bind('e', turn_right())
+
+
+	main.mainloop()
+
+class DroneINIT():
+	drone = ps_drone.Drone()
+	#drone.startup()
+
+	Controller()
+
+
+
+if __name__ == "__main__":
+	DroneINIT()	
