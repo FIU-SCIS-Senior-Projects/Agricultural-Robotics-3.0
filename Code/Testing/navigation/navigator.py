cffb161977ffb8c96b2cf12dd12a6b827920fd73
Sean Monroe <seanmonroe86@gmail.com>
2017-06-02 12:13:43 -0400

    Comments, and refined movement calculation

--- Code/Testing/navigation/navigator.py
+++ Code/Testing/navigation/navigator.py
@@ -2,10 +2,14 @@ import ps_drone, time, math
 
 class Navigator:
     """Navigator interface of an AR Drone 2.0"""
+    SOFT_TURN = 0.2
+    HARD_TURN = 1.0
 
     def __init__(self, drone):
         """Initialize drone navigation variables"""
         req_packages = ["altitude", "gps", "magneto", "raw_measures"]
+
+        # Default (invalid) field values
         self.__mag_avg = [-14, 13] # Manually calculated
         self.__mag_acc = 6
         self.__tar_gps = [0.0, 0.0]
@@ -13,10 +17,15 @@ class Navigator:
         self.__tar_angle = 0.0
         self.__stats = {}
 
+        # Initialize sensor data transmissions
         self.__drone = drone
         self.__drone.useDemoMode(False)
         self.__drone.getNDpackage(req_packages)
+
+        # Populate default field vals with real ones
         self.__set_stats()
+
+        # Get current GPS for "home" location
         self.__home = self.__stats["gps"]
 
     def __calc_distance(self):
@@ -24,11 +33,14 @@ class Navigator:
         r = 6371e3  # earth's radius in m
         x = self.__stats["gps"]
         y = self.__tar_gps
+
+        # Convert GPS degrees to radians
         phi1 = math.radians(x[0])
         phi2 = math.radians(y[0])
         dphi = math.radians(y[0] - x[0])
         dlam = math.radians(y[1] - x[1])
     
+        # 'Great circle' distance between two GPS coords
         a = math.sin(dphi / 2) * math.sin(dphi / 2)
         a += math.cos(phi1) * math.cos(phi2)
         a *= (math.sin(dlam / 2) * math.sin(dlam / 2))
@@ -38,6 +50,8 @@ class Navigator:
         """Calculate necessary heading for straight flight to target"""
         x = self.__stats["gps"]
         y = self.__tar_gps
+
+        # Initial heading required for 'Great circle' traversal
         q = math.sin(y[1] - x[1]) * math.cos(y[0])
         p = math.cos(x[0]) * math.sin(y[0])
         p -= math.sin(x[0]) * math.cos(y[0]) * math.cos(y[1] - x[1])
@@ -57,20 +71,19 @@ class Navigator:
         stats["gps"] = self.__drone.NavData["gps"][:-1] # not using altitude value
 
         # Convert altitude to meters
-        alt = self.__drone.NavData["altitude"][0] # mm
-        stats["alt"] = alt / 1000.0 # m
+        stats["alt"] = self.__drone.NavData["altitude"][0] / 1000.0
     
         # Turn magnetometer data into heading (degrees)
         stats["mag"] = self.__drone.NavData["magneto"][0][:-1] # not using z value
         for i in range(len(stats["mag"])): stats["mag"][i] -= self.__mag_avg[i]
-        stats["deg"] = -1 * (math.atan2(
-            stats["mag"][1], stats["mag"][0]) * 180) / math.pi
-        if stats["deg"] < 0: stats["deg"] = stats["deg"] + 360
+        stats["deg"] = (360 + (-1 * (math.atan2(
+            stats["mag"][1], stats["mag"][0]) * 180) / math.pi)) % 360
+
+        # Set new stats
         self.__stats = stats
     
     def __calc_mag(self):
-        """Rotates the drone to acquire mag data to normalize."""
-        # UNTESTED
+        """Rotates the drone to acquire mag data to use in normalization."""
         mag_x, mag_y = [], []
         for i in range(self.__mag_acc):
             NDC = self.__drone.NavDataCount
@@ -78,14 +91,14 @@ class Navigator:
             mag = self.__drone.NavData["magneto"]
             mag_x.append(mag[0])
             mag_y.append(mag[1])
-            self.__drone.turnAngle((360.0 / self.__mag_acc), 0.5)
-            time.sleep(2)
+            self.__drone.turnAngle((360.0 / self.__mag_acc), 1.0)
+            time.sleep(3)
         self.__mag_avg[0] = reduce(lambda x, y: x + y, mag_x) / len(mag_x)
         self.__mag_avg[1] = reduce(lambda x, y: x + y, mag_y) / len(mag_y)
     
     def calibrate_drone(self, *mag):
         """Basic gyroscope and magnetometer recalibration."""
-        # Requires 10 seconds of hovering flight.
+        # Requires 10-15 seconds of hovering flight.
         self.__drone.trim()
         time.sleep(5)
         self.__drone.takeoff()
@@ -101,22 +114,28 @@ class Navigator:
         """Perform calculations to get arguments for a drone move"""
         self.__set_stats()
 
-        # Check if drone needs drastic turn
+        # Get angle of required turn
         self.__calc_heading()
         self.__calc_distance()
         angle_diff = self.__stats["deg"] - self.__tar_angle
         if angle_diff > 180: angle_turn = angle_diff - 360
         else: angle_turn = angle_diff
 
-        if   angle_turn >  10.0: return [0.0, 0.0, 0.0,  0.5, self.__tar_dist]
-        elif angle_turn < -10.0: return [0.0, 0.0, 0.0, -0.5, self.__tar_dist]
-        elif angle_turn > 0: turn_speed =  0.2
-        elif angle_turn < 0: turn_speed = -0.2
+        # If drastic turn is needed, only perform that turn
+        if   angle_turn >  10.0: move_speed, turn_speed = 0.0, -HARD_TURN
+        elif angle_turn < -10.0: move_speed, turn_speed = 0.0,  HARD_TURN
+        elif angle_turn > 0: move_speed, turn_speed = 0.5, -SOFT_TURN
+        elif angle_turn < 0: move_speed, turn_speed = 0.5,  SOFT_TURN
         else: turn_speed = 0.0
 
-        # Otherwise move forward with small turn, return distance
-        return [0.25, 0.0, 0.0, turn_speed, self.__tar_dist]
+        # Return movement list and distance to target
+        return ([move_speed, 0.0, 0.0, turn_speed], self.__tar_dist)
 
+    def set_target(self, new_target):
+        self.__tar_gps = new_target
+
+
+    # Diagnostic functions
     def get_home(self):
         return self.__home
 
@@ -127,8 +146,5 @@ class Navigator:
     def get_deg(self):
         self.__set_stats()
         return self.__stats["deg"]
-
-    def set_target(self, new_target):
-        self.__tar_gps = new_target
 
 
