bd8121b543abc9ad6da456f70829f0a1b50f6514
Sean Monroe <seanmonroe86@gmail.com>
2017-06-05 13:06:55 -0400

    introduced outlier removal for sensor data

--- Code/Testing/navigation/navigator.py
+++ Code/Testing/navigation/navigator.py
@@ -1,4 +1,4 @@
-import ps_drone, time, math
+import itertools, ps_drone, time, math
 
 class Navigator:
     """Navigator interface of an AR Drone 2.0"""
@@ -12,6 +12,7 @@ class Navigator:
         # Default (invalid) field values
         self.__mag_avg = [-14, 13] # Manually calculated
         self.__mag_acc = 6
+        self.__samples = []
         self.__tar_gps = [0.0, 0.0]
         self.__tar_dist = 0.0
         self.__tar_angle = 0.0
@@ -21,13 +22,127 @@ class Navigator:
         self.__drone = drone
         self.__drone.useDemoMode(False)
         self.__drone.getNDpackage(req_packages)
+        time.sleep(0.1)
 
-        # Populate default field vals with real ones
-        self.__set_stats()
+        # Start taking sensor data
+        self.__sensors = Thread(target=self.__sensors_collect, args=())
+        self.__sensors.daemon = True
+        self.__sensors.start()
+        time.sleep(0.1)
 
         # Get current GPS for "home" location
+        self.__set_stats()
         self.__home = self.__stats["gps"]
 
+    def __sensors_collect(self):
+        """Continuously collects sensor data"""
+        # Initialize samples
+        for i in range(4):
+            self.__samples.append(self.__get_stats())
+            time.sleep(0.5)
+
+        while True:
+            # If there are too many samples, delete some
+            while len(self.__samples) >= 5: del self.__samples[0]
+
+            # Add new sensor data
+            self.__samples.append(self.__get_stats())
+
+            # Wait for next sampling
+            time.sleep(0.5)
+
+    def __set_stats(self):
+        """Preprocessing of stats queue to reduce variation"""
+        acc, gyr, gps, alt, mag, deg = [], [], [], [], [], []
+        for item in self.__samples:
+            acc.append(item["acc"])
+            gyr.append(item["gyr"])
+            gps.append(item["gps"])
+            alt.append(item["alt"])
+            mag.append(item["mag"])
+            deg.append(item["deg"])
+
+        # Remove outliers
+        acc_proc = list(itertools.compress(acc, is_outlier(acc)))
+        gyr_proc = list(itertools.compress(gyr, is_outlier(gyr)))
+        gps_proc = list(itertools.compress(gps, is_outlier(gps)))
+        alt_proc = list(itertools.compress(alt, is_outlier(alt)))
+        mag_proc = list(itertools.compress(mag, is_outlier(mag)))
+        deg_proc = list(itertools.compress(deg, is_outlier(deg)))
+
+        # Check that lists are populated
+        if not acc_proc: acc_proc = acc
+        if not gyr_proc: gyr_proc = gyr
+        if not gps_proc: gps_proc = gps
+        if not alt_proc: alt_proc = alt
+        if not mag_proc: mag_proc = mag
+        if not deg_proc: deg_proc = deg
+
+        # Average the remainder of the lists
+        self.__stats["acc"] = reduce(lambda x, y: x + y, acc) / len(acc)
+        self.__stats["gyr"] = reduce(lambda x, y: x + y, gyr) / len(gyr)
+        self.__stats["gps"] = reduce(lambda x, y: x + y, gps) / len(gps)
+        self.__stats["alt"] = reduce(lambda x, y: x + y, alt) / len(alt)
+        self.__stats["mag"] = reduce(lambda x, y: x + y, mag) / len(mag)
+        self.__stats["deg"] = reduce(lambda x, y: x + y, deg) / len(deg)
+
+    def is_outlier(points, thresh=3.5):
+        """
+        Returns a boolean array with True if points are outliers and False 
+        otherwise.
+    
+        Parameters:
+        -----------
+            points : An numobservations by numdimensions array of observations
+            thresh : The modified z-score to use as a threshold. Observations with
+                a modified z-score (based on the median absolute deviation) greater
+                than this value will be classified as outliers.
+    
+        Returns:
+        --------
+            mask : A numobservations-length boolean array.
+    
+        References:
+        ----------
+            Boris Iglewicz and David Hoaglin (1993), "Volume 16: How to Detect and
+            Handle Outliers", The ASQC Basic References in Quality Control:
+            Statistical Techniques, Edward F. Mykytka, Ph.D., Editor. 
+        """
+        if len(points.shape) == 1:
+            points = points[:,None]
+        median = np.median(points, axis=0)
+        diff = np.sum((points - median)**2, axis=-1)
+        diff = np.sqrt(diff)
+        med_abs_deviation = np.median(diff)
+    
+        modified_z_score = 0.6745 * diff / med_abs_deviation
+    
+        return modified_z_score > thresh
+
+    def __get_stats(self):
+        """Get stats list with human-readable sensor data."""
+        stats = {}
+        # Get fresh NavData
+        NDC = self.__drone.NavDataCount
+        while self.__drone.NavDataCount == NDC: time.sleep(0.01)
+    
+        # Straightforward data
+        stats["acc"] = self.__drone.NavData["raw_measures"][0]
+        stats["gyr"] = self.__drone.NavData["raw_measures"][1]
+        stats["gps"] = self.__drone.NavData["gps"][:-1] # not using altitude value
+
+        # Convert altitude to meters
+        stats["alt"] = self.__drone.NavData["altitude"][0] / 1000.0
+    
+        # Turn magnetometer data into heading (degrees)
+        stats["mag"] = self.__drone.NavData["magneto"][0][:-1] # not using z value
+        for i in range(len(stats["mag"])): stats["mag"][i] -= self.__mag_avg[i]
+        stats["deg"] = (360 + (-1 * (math.atan2(
+            stats["mag"][1], stats["mag"][0]) * 180) / math.pi)) % 360
+
+        # Set new stats
+        return stats
+    
     def __calc_distance(self):
         """Calculate distance to target"""
         r = 6371e3  # earth's radius in m
@@ -57,30 +172,6 @@ class Navigator:
         p -= math.sin(x[0]) * math.cos(y[0]) * math.cos(y[1] - x[1])
         b = math.atan2(q, p) * 180.0 / math.pi
         self.__tar_angle = (b + 360.0) % 360.0
-    
-    def __set_stats(self):
-        """Set stats list with human-readable sensor data."""
-        stats = {}
-        # Get fresh NavData
-        NDC = self.__drone.NavDataCount
-        while self.__drone.NavDataCount == NDC: time.sleep(0.01)
-    
-        # Straightforward data
-        stats["acc"] = self.__drone.NavData["raw_measures"][0]
-        stats["gyr"] = self.__drone.NavData["raw_measures"][1]
-        stats["gps"] = self.__drone.NavData["gps"][:-1] # not using altitude value
-
-        # Convert altitude to meters
-        stats["alt"] = self.__drone.NavData["altitude"][0] / 1000.0
-    
-        # Turn magnetometer data into heading (degrees)
-        stats["mag"] = self.__drone.NavData["magneto"][0][:-1] # not using z value
-        for i in range(len(stats["mag"])): stats["mag"][i] -= self.__mag_avg[i]
-        stats["deg"] = (360 + (-1 * (math.atan2(
-            stats["mag"][1], stats["mag"][0]) * 180) / math.pi)) % 360
-
-        # Set new stats
-        self.__stats = stats
     
     def __calc_mag(self):
         """Rotates the drone to acquire mag data to use in normalization."""
@@ -146,5 +237,9 @@ class Navigator:
     def get_deg(self):
         self.__set_stats()
         return self.__stats["deg"]
+
+    def get_all(self):
+        self.__set_stats()
+        return self.__stats
 
 
