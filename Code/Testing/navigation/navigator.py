519b2ce22b8837b1023b30b87b9a7520d014a332
Sean Monroe <seanmonroe86@gmail.com>
2017-06-06 11:49:59 -0400

    debug corrections

--- Code/Testing/navigation/navigator.py
+++ Code/Testing/navigation/navigator.py
@@ -5,33 +5,38 @@ np.seterr(divide='ignore', invalid='ignore')
 
 class Navigator:
     """Navigator interface of an AR Drone 2.0"""
-    SOFT_TURN = 0.2
-    HARD_TURN = 1.0
 
     def __init__(self, drone):
         """Initialize drone navigation variables"""
-        req_packages = ["altitude", "gps", "magneto", "raw_measures"]
+        # Constants
+        self.__REQ_PACKS = ["altitude", "demo", "gps", "magneto", "raw_measures"]
+        self.__SOFT_TURN = 0.1
+        self.__HARD_TURN = 0.3
+        self.__DEF_SPD   = 0.3
+        self.__SAMP_NUM  = 10
+        self.__SAMP_TIME = 1.0
 
         # Default (invalid) field values
         self.__mag_avg = [-14, 13] # Manually calculated
-        self.__mag_acc = 6
-        self.__samples = []
-        self.__tar_gps = [0.0, 0.0]
+        #self.__mag_avg = [-42.7222, 9.2991] # Manually calculated
+        self.__mag_acc = 6  # Points to record during calibration
+        self.__samples = [] # Sample queue
+        self.__tar_gps = [0.0, 0.0] # Target's gps coordinate
         self.__tar_dist = 0.0
         self.__tar_angle = 0.0
-        self.__stats = {}
+        self.__stats = {}   # Stats dict
 
         # Initialize sensor data transmissions
         self.__drone = drone
         self.__drone.useDemoMode(False)
-        self.__drone.getNDpackage(req_packages)
+        self.__drone.getNDpackage(self.__REQ_PACKS)
         time.sleep(0.1)
 
         # Start taking sensor data
         self.__sensors = Thread(target=self.__sensors_collect, args=())
         self.__sensors.daemon = True
         self.__sensors.start()
-        time.sleep(3)
+        time.sleep(self.__SAMP_TIME * self.__SAMP_NUM)
 
         # Get current GPS for "home" location
         self.__set_stats()
@@ -42,17 +47,18 @@ class Navigator:
         # Initialize samples
         for i in range(4):
             self.__samples.append(self.__get_stats())
-            time.sleep(0.5)
+            time.sleep(self.__SAMP_TIME)
 
         while True:
             # If there are too many samples, delete some
-            while len(self.__samples) >= 5: del self.__samples[0]
+            while len(self.__samples) >= self.__SAMP_NUM:
+                del self.__samples[0]
 
             # Add new sensor data
             self.__samples.append(self.__get_stats())
 
             # Wait for next sampling
-            time.sleep(0.5)
+            time.sleep(self.__SAMP_TIME)
 
     def __set_stats(self):
         """Preprocessing of stats queue to reduce variation"""
@@ -65,35 +71,35 @@ class Navigator:
             mag.append(item["mag"])
             deg.append(item["deg"])
 
-        # Remove outliers
-        acc_proc = list(itertools.compress(
-            acc, self.__is_outlier(np.array(acc))))
-        gyr_proc = list(itertools.compress(gyr,
-            self.__is_outlier(np.array(gyr))))
-        gps_proc = list(itertools.compress(gps,
-            self.__is_outlier(np.array(gps))))
-        alt_proc = list(itertools.compress(alt,
-            self.__is_outlier(np.array(alt))))
-        mag_proc = list(itertools.compress(mag,
-            self.__is_outlier(np.array(mag))))
-        deg_proc = list(itertools.compress(deg,
-            self.__is_outlier(np.array(deg))))
-
-        # Check that lists are populated
-        if not acc_proc: acc_proc = acc
-        if not gyr_proc: gyr_proc = gyr
-        if not gps_proc: gps_proc = gps
-        if not alt_proc: alt_proc = alt
-        if not mag_proc: mag_proc = mag
-        if not deg_proc: deg_proc = deg
+#        # Remove outliers
+#        acc_proc = list(itertools.compress(
+#            acc, self.__is_outlier(np.array(acc))))
+#        gyr_proc = list(itertools.compress(gyr,
+#            self.__is_outlier(np.array(gyr))))
+#        gps_proc = list(itertools.compress(gps,
+#            self.__is_outlier(np.array(gps))))
+#        alt_proc = list(itertools.compress(alt,
+#            self.__is_outlier(np.array(alt))))
+#        mag_proc = list(itertools.compress(mag,
+#            self.__is_outlier(np.array(mag))))
+#        deg_proc = list(itertools.compress(deg,
+#            self.__is_outlier(np.array(deg))))
+#
+#        # Check that lists are populated
+#        if not acc_proc: acc_proc = acc
+#        if not gyr_proc: gyr_proc = gyr
+#        if not gps_proc: gps_proc = gps
+#        if not alt_proc: alt_proc = alt
+#        if not mag_proc: mag_proc = mag
+#        if not deg_proc: deg_proc = deg
 
         # Average the remainder of the lists
-        self.__stats["acc"] = reduce(lambda x, y: x + y, np.array(acc_proc)) / len(acc_proc)
-        self.__stats["gyr"] = reduce(lambda x, y: x + y, np.array(gyr_proc)) / len(gyr_proc)
-        self.__stats["gps"] = reduce(lambda x, y: x + y, np.array(gps_proc)) / len(gps_proc)
-        self.__stats["alt"] = reduce(lambda x, y: x + y, np.array(alt_proc)) / len(alt_proc)
-        self.__stats["mag"] = reduce(lambda x, y: x + y, np.array(mag_proc)) / len(mag_proc)
-        self.__stats["deg"] = reduce(lambda x, y: x + y, np.array(deg_proc)) / len(deg_proc)
+        self.__stats["acc"] = reduce(lambda x, y: x + y, np.array(acc)) / len(acc)
+        self.__stats["gyr"] = reduce(lambda x, y: x + y, np.array(gyr)) / len(gyr)
+        self.__stats["gps"] = reduce(lambda x, y: x + y, np.array(gps)) / len(gps)
+        self.__stats["alt"] = reduce(lambda x, y: x + y, np.array(alt)) / len(alt)
+        self.__stats["mag"] = reduce(lambda x, y: x + y, np.array(mag)) / len(mag)
+        self.__stats["deg"] = reduce(lambda x, y: x + y, np.array(deg)) / len(deg)
 
     def __is_outlier(self, points, thresh=3.5):
         """
@@ -192,10 +198,13 @@ class Navigator:
             mag = self.__drone.NavData["magneto"]
             mag_x.append(mag[0])
             mag_y.append(mag[1])
-            self.__drone.turnAngle((360.0 / self.__mag_acc), 1.0)
-            time.sleep(3)
-        self.__mag_avg[0] = reduce(lambda x, y: x + y, mag_x) / len(mag_x)
-        self.__mag_avg[1] = reduce(lambda x, y: x + y, mag_y) / len(mag_y)
+            self.__drone.turnAngle(-(360.0 / self.__mag_acc), 1.0)
+            self.__drone.hover()
+            time.sleep(2)
+        #self.__mag_avg[0] = reduce(lambda x, y: x + y, mag_x) / len(mag_x)
+        #self.__mag_avg[1] = reduce(lambda x, y: x + y, mag_y) / len(mag_y)
+        self.__mag_avg[0] = np.mean(np.array(mag_x))
+        self.__mag_avg[1] = np.mean(np.array(mag_y))
     
     def calibrate_drone(self, *mag):
         """Basic gyroscope and magnetometer recalibration."""
@@ -218,16 +227,20 @@ class Navigator:
         # Get angle of required turn
         self.__calc_heading()
         self.__calc_distance()
-        angle_diff = self.__stats["deg"] - self.__tar_angle
-        if angle_diff > 180: angle_turn = angle_diff - 360
-        else: angle_turn = angle_diff
+        angle_diff = self.__drone.angleDiff(
+                self.__stats["deg"], self.__tar_angle)
 
         # If drastic turn is needed, only perform that turn
-        if   angle_turn >  10.0: move_speed, turn_speed = 0.0, -HARD_TURN
-        elif angle_turn < -10.0: move_speed, turn_speed = 0.0,  HARD_TURN
-        elif angle_turn > 0: move_speed, turn_speed = 0.5, -SOFT_TURN
-        elif angle_turn < 0: move_speed, turn_speed = 0.5,  SOFT_TURN
-        else: move_speed, turn_speed = 0.5, 0.0
+        if   angle_diff >  10.0:
+            move_speed, turn_speed = 0.0,           -self.__HARD_TURN
+        elif angle_diff < -10.0:
+            move_speed, turn_speed = 0.0,            self.__HARD_TURN
+        elif angle_diff > 0:
+            move_speed, turn_speed = self.__DEF_SPD,  -self.__SOFT_TURN
+        elif angle_diff < 0:
+            move_speed, turn_speed = self.__DEF_SPD,   self.__SOFT_TURN
+        else:
+            move_speed, turn_speed = self.__DEF_SPD,   0.0
 
         # Return movement list and distance to target
         return ([move_speed, 0.0, 0.0, turn_speed], self.__tar_dist)
@@ -239,6 +252,9 @@ class Navigator:
     # Diagnostic functions
     def get_home(self):
         return self.__home
+
+    def set_home(self, new_home):
+        self.__home = new_home
 
     def get_mag(self):
         self.__set_stats()
