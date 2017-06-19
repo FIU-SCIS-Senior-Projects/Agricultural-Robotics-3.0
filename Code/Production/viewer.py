a45487ddd0a5c1cf5cb68f028314352130cd985d
Sean Monroe <seanmonroe86@gmail.com>
2017-06-19 14:59:08 -0400

    enabling image manipulation

--- Code/Production/viewer.py
+++ Code/Production/viewer.py
@@ -17,6 +17,11 @@ class Camera:
         self.__capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,self.__CAMERA_WIDTH)
         self.__capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT,self.__CAMERA_HEIGHT)
 
+        # Configure computer vision
+        self.__edges = False
+        self.__corners = False
+        self.__colors = {"on": False,"colors": []}
+
     def start(self):
         cam = Thread(target=self.__updateFrame, args=())
         cam.daemon = True
@@ -27,8 +32,16 @@ class Camera:
             ret, self.__currentFrame = self.__capture.read()
             while not ret: ret, frame = self.__capture.read()
 
+    def __make_mono(self, img):
+        return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
     def getFrame(self):
-        return self.__currentFrame
+        out_image = self.__currentFrame
+        if self.__mono: out_image = self.__make_mono(out_image)
+        if self.__edges: out_image = self.__make_edges(out_image)
+        if self.__corners: out_image = self.__make_corners(out_image)
+        if self.__colors["on"]: out_image = self.__make_colors(out_image)
+        return out_image
 
     def release(self):
         return self.__capture.release()
