c4071c7483e1c89aa2a04fd736c8198d730f9b87
Sean Monroe <seanmonroe86@gmail.com>
2017-06-23 12:39:08 -0400

    small mod to constructor messages

--- Code/Production/navigator.py
+++ Code/Production/navigator.py
@@ -49,7 +49,7 @@ class Navigator:
         self.__home = self.__stats["gps"]
 
         # Done initializing
-        print ">>> READY"
+        print ">>> NAVIGATOR READY"
 
     def __sensors_collect(self):
         """Continuously collects sensor data"""
@@ -108,8 +108,9 @@ class Navigator:
         stats = {}
         # Get fresh NavData
         NDC = self.__drone.NavDataCount
-        while self.__drone.NavDataCount == NDC: time.sleep(0.01)
-    
+        while self.__drone.NavDataCount == NDC or
+        not all(package in self.__drone.NavData for package in self.__REQ_PACKS): time.sleep(0.01)
+
         # Straightforward data
         stats["acc"] = self.__drone.NavData["raw_measures"][0]
         stats["gyr"] = self.__drone.NavData["raw_measures"][1]
