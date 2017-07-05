981215001295ae1ef348f4d88d2f21d7f2ba0f56
Sean Monroe <seanmonroe86@gmail.com>
2017-07-05 12:07:54 -0400

    new loc of main func, win consts

---
+++ Code/Production/AgriDrone.py
@@ -0,0 +1,18 @@
+from gui import DCMainApp
+import Tkinter as tk
+
+WIN_WIDTH = 880
+WIN_HEIGHT = 760
+
+def main():
+    # Initialize GUI
+    root = tk.Tk()
+    root.geometry("{}x{}".format(WIN_WIDTH, WIN_HEIGHT)) #GUI window dimensions
+    drone_GUI = DCMainApp(root, WIN_WIDTH, WIN_HEIGHT)
+
+    # Run GUI
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
