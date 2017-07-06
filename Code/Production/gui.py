b63479b1a4ec5c293e4c539177381fb245321047
Sean Monroe <seanmonroe86@gmail.com>
2017-07-06 14:00:55 -0400

    corrected min/max lat bug

--- Code/Production/gui.py
+++ Code/Production/gui.py
@@ -66,9 +66,9 @@ class DCMainApp(object):
         # Internal storage of map tiles will discard need for an active internet connection.
         self.LAT     = 25.759027         #Center latitude of staticmap image
         self.LONG    = -80.374598        #Center longitude of staticmap image
-        self.MINLAT  = 25.758544         #Lower bound staticmap image latitude
+        self.MAXLAT  = 25.758544         #Lower bound staticmap image latitude
         self.MINLONG = -80.375419        #Lower bound staticmap image longitude
-        self.MAXLAT  = 25.759510         #Upper bound staticmap image latitude
+        self.MINLAT  = 25.759510         #Upper bound staticmap image latitude
         self.MAXLONG = -80.373815        #Upper bound staticmap image longitude
 
         # Pixel width calculation dx and dy
@@ -613,6 +613,7 @@ class DCMainApp(object):
 
         self.getlat =  ((self.clk_pix_y * (self.MAXLAT - self.MINLAT))/(self.map_height-0)) + self.MINLAT
         self.getlong = ((self.clk_pix_x * (self.MAXLONG - self.MINLONG))/(self.map_width-0)) + self.MINLONG
+        print "lat: {}\nlon: {}".format(self.getlat, self.getlong)
 
         self.clk_arr.append([event.x, event.y]) # List of marker pixel locations
         self.pix_gps_coor.append([self.getlat,self.getlong]) #List of GPS locations
